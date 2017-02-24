6ad2f999740beb823163df126b34dcc8c5c17465
Sergey-Tihomirov <tixomirov95@yandex.ru>
2017-02-24 21:29:23 +0300

    --

---
+++ WindowsFormsApplication8/WindowsFormsApplication8/Classes/CheckString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace WindowsFormsApplication8
+{
+    class CheckString
+    {
+        static public void checkStringWithDouble(System.Windows.Forms.KeyPressEventArgs e)
+        {
+            if (Char.IsNumber(e.KeyChar) | (e.KeyChar == Convert.ToChar(",")) | (e.KeyChar == '\b'))
+            {
+                return;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
---
+++ WindowsFormsApplication8/WindowsFormsApplication8/Classes/EventKeyPressForTextBox.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+
+namespace WindowsFormsApplication8
+{
+    class EventKeyPressForTextBox
+    {
+        public static void keyPressForTextBoxWithDouble(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == '.')
+            {
+                e.KeyChar = ',';
+                keyPressForTextBoxWithDouble(sender, e);
+            }
+            CheckString.checkStringWithDouble(e);
+        }
+    }
+}
