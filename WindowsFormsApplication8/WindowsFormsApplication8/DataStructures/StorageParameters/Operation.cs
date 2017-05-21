05bbc37a099b8384c3ae168b0dc6a1647cf6a769
Sergey-Tihomirov <tixomirov95@yandex.ru>
2017-05-21 22:40:40 +0300

    Для малого списка операций добавил класс

---
+++ WindowsFormsApplication8/WindowsFormsApplication8/DataStructures/StorageParameters/Operation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication8
+{
+    public class Operation
+    {
+        private string nameOperation;
+        private int idOperation;
+        private string typeOfInstrument;
+
+        public Operation(string nameOperation, int idOperation)
+        {
+            this.nameOperation = nameOperation;
+            this.idOperation = idOperation;
+        }
+
+        public string getNameOperation()
+        {
+            return nameOperation;
+        }
+
+        public int getIdOperation()
+        {
+            return idOperation;
+        }
+
+        public void setTypeOfInstrument(string typeOfInstrument)
+        {
+            this.typeOfInstrument = typeOfInstrument;
+        }
+
+        public string getTypeOfInstrument()
+        {
+            return typeOfInstrument;
+        }
+    }
+}
